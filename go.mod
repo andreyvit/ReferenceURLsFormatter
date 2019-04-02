41791bb37a5ce4c897978e8e4154885e1e161ce1
Andrey Tarantsov <andrey@tarantsov.com>
2019-04-02 13:49:18 +0300

    Initial version

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/andreyvit/ReferenceURLsFormatter
+
+go 1.12
+
+require (
+	github.com/badoux/goscraper v0.0.0-20181207103713-9b4686c4b62c
+	github.com/gogo/protobuf v1.2.1 // indirect
+	github.com/hashicorp/golang-lru v0.5.1 // indirect
+	github.com/mvdan/xurls v1.1.0 // indirect
+	github.com/neelance/parallel v0.0.0-20160708114440-4de9ce63d14c // indirect
+	github.com/opentracing/basictracer-go v1.0.0 // indirect
+	github.com/opentracing/opentracing-go v1.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_golang v0.9.2 // indirect
+	github.com/slimsag/godocmd v0.0.0-20161025000126-a1005ad29fe3 // indirect
+	github.com/sourcegraph/ctxvfs v0.0.0-20180418081416-2b65f1b1ea81 // indirect
+	github.com/sourcegraph/go-langserver v2.0.0+incompatible // indirect
+	github.com/sourcegraph/jsonrpc2 v0.0.0-20190106185902-35a74f039c6a // indirect
+	golang.org/x/net v0.0.0-20190328230028-74de082e2cca // indirect
+	golang.org/x/tools v0.0.0-20190401205534-4c644d7e323d // indirect
+	mvdan.cc/xurls v1.1.0
+	mvdan.cc/xurls/v2 v2.0.0
+)
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/badoux/goscraper"
+	"mvdan.cc/xurls/v2"
+)
+
+// var (
+// 	indexRe = regexp.MustCompile(`^\d+[.)]\s*`)
+// )
+
+func main() {
+	err := run()
+	if err != nil {
+		log.Fatalf("** %+v", err)
+	}
+}
+
+func run() error {
+	data, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		return err
+	}
+	lines := strings.Split(string(data), "\n")
+
+	urlsRe := xurls.Relaxed()
+
+	today := time.Now().Format("02.01.2006")
+
+	output, err := os.Create("output.txt")
+	if err != nil {
+		return err
+	}
+	defer output.Close()
+
+	errors, err := os.Create("errors.txt")
+	if err != nil {
+		return err
+	}
+	defer errors.Close()
+
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if len(line) == 0 {
+			continue
+		}
+
+		// line = indexRe.ReplaceAllLiteralString(line, "")
+
+		urls := urlsRe.FindAllString(line, -1)
+		if len(urls) == 0 {
+			log.Printf("No URLs found in: %v", line)
+		}
+		for _, url := range urls {
+			if !strings.Contains(url, "://") {
+				url = "http://" + url
+			}
+
+			var title string
+
+			s, err := goscraper.Scrape(url, 5)
+			if err != nil {
+				fmt.Fprintf(errors, "Cannot download %v : %v\n", url, err)
+				title = "ОШИБКА ПРИ ПОЛУЧЕНИИ ЗАГОЛОВКА"
+			} else {
+				title = strings.TrimSpace(s.Preview.Title)
+			}
+
+			fmt.Fprintf(output, "%s [Электронный ресурс]. – Режим доступа: %s. – Заглавие с экрана. – (Дата обращения: %s).\n", title, url, today)
+		}
+
+	}
+
+	return nil
+}
